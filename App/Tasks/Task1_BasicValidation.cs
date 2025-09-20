b7f58096ebf75e575b8ffa206a4a447292ad8b07
user1 <user1@DESKTOP-UNR49SD>
2025-09-20 12:24:27 +0300

    задание 1

--- App/Tasks/Task1_BasicValidation.cs
+++ App/Tasks/Task1_BasicValidation.cs
@@ -4,7 +4,22 @@ public static class BasicValidation
 {
     public static int ParseStringToInt(string input)
     {
-        return 0;
+        try
+        {
+            return int.Parse(input);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("error");
+            return 0;
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("");
+            return 0;
+        }
+
+
     }
 }
 
