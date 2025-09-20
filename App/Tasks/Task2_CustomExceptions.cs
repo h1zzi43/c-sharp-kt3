6b12ec54a0411a4b7992208959cef5dca6b0a204
user1 <user1@DESKTOP-UNR49SD>
2025-09-20 12:47:42 +0300

    задание 2

--- App/Tasks/Task2_CustomExceptions.cs
+++ App/Tasks/Task2_CustomExceptions.cs
@@ -1,9 +1,36 @@
+
 namespace App.Tasks;
+
+public class InvalidPasswordException : Exception
+{
+    public InvalidPasswordException() : base() { }
+
+    public InvalidPasswordException(string message) : base(message) { }
+
+    public InvalidPasswordException(string message, Exception innerException) : base(message, innerException) { }
+}
 
 public static class PasswordValidator
 {
+   
     public static void ValidatePassword(string password)
     {
+
+      if (password == null)
+        {
+            throw new InvalidPasswordException("Пароль не должен быть null");
+        }
+
+      if (password.Length < 8)
+        {
+            throw new InvalidPasswordException("Длина пароля не менее 8 символов");
+        }
+        if (!password.Any(char.IsDigit)) 
+        {
+            throw new InvalidPasswordException("Пароль должен содержать хотя бы одну цифру");
+        }
     }
+
+    
 }
 
